314bdc07bf6c0d4f286bf8d3610b4c8a9faeece7
flsouto <flsouto666@gmail.com>
2022-08-15 23:29:43 -0300

    add keyboard background fx

--- src/test_render.ts
+++ src/test_render.ts
@@ -22,10 +22,13 @@ async function main(){
 
     let say = "I'm going to start by adding the basic structure of our HTML page. It looks boring at first, but hang in there, as this is going to become more complex in a minute!"
     execSync(`echo "${say}" | text2wave > results/say.wav`)
-    const dur = parseFloat(execSync(`soxi -D results/say.wav`))
+    const dur = parseFloat(execSync(`soxi -D results/say.wav`)) + 1
+    execSync(`sox results/say.wav results/say-fixed.wav rate 44100`)
+    execSync(`sox assets/keyboard-fx.wav results/keyboard.wav trim 0 ${dur}`)
+    execSync(`sox -M results/keyboard.wav results/say-fixed.wav results/say-with-keyboard.wav remix -m 1,3,2,3`)
     const vr = new VideoRenderer()
     vr.setFrameRate(imgs.length/dur)
-    vr.setAudio(`results/say.wav`)
+    vr.setAudio(`results/say-with-keyboard.wav`)
     vr.render("results/*.png")
 
 }
